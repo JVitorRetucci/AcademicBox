fb1feeb4ac64865d0995bf7c02fb6316f945630f
Henrique Canella <henrique.mandelli@gmail.com>
2019-12-02 16:42:12 -0300

    Correção pesquisa de materiais frontend

---
+++ frontend/src/pages/content/content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from "../../components/header/header";
+
+// import { Container } from './styles';
+
+export default function content() {
+  return (
+    <Header></Header>
+  );
+}
