bf2cac72079345fcf76caf5ad568d975eb36840e
Henrique Canella <henrique.mandelli@gmail.com>
2019-12-03 15:55:56 -0300

    Correção exibição de comentarios backend

--- backend/src/app/controllers/SpecificContentController.js
+++ backend/src/app/controllers/SpecificContentController.js
@@ -1,14 +1,19 @@
 import Content from '../models/Content';
+import Comment from '../models/Comment';
 
 class SpecificContentController {
   async index(req, res) {
     const content = await Content.findByPk(req.params.id);
 
+    const comment = await Comment.findAll({
+      where: { comentario_conteudo_id: req.params.id },
+    });
+
     if (!content) {
       return res.status(400).json({ error: 'Content does not exists' });
     }
 
-    return res.json(content);
+    return res.json({ content, comment });
   }
 }
 
