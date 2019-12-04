71344bb1eef66136ec8337dea28f0f0fd7242cb3
João Vítor Retucci <jvitor.200003@gmail.com>
2019-12-04 14:29:24 -0300

    Users Helper

---
+++ frontend/src/helpers/users.js
@@ -0,0 +1,11 @@
+export const getUserName = () => {
+    const user = JSON.parse(localStorage.getItem('logged-user'));
+
+    return user.usuario_nome;
+}
+
+export const getUserXP = () => {
+    const user = JSON.parse(localStorage.getItem('logged-user'));
+
+    return user.usuario_xp;
+}
