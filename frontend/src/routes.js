9e90555452b39ddf9e97e685b1947849ccae3439
João Vítor Retucci <jvitor.200003@gmail.com>
2019-12-04 09:52:23 -0300

    Added Register Page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,6 +6,7 @@ import Home from './pages/home/home.js';
 import Main from './pages/main/main.js';
 import Suggestion from './pages/suggestion/suggestion.js';
 import Content from './pages/content/content.js';
+import Register from './pages/register/register.js';
 
 const Routes = () => (
   <BrowserRouter>
@@ -13,6 +14,7 @@ const Routes = () => (
       <Route exact path="/" component={Home} />
       <Route path="/main" component={Main} />
       <Route path="/suggestion" component={Suggestion} />
+      <Route path="/register" component={Register} />
       <Route path="/contents/:id" component={Content} />
     </Switch>
   </BrowserRouter>
